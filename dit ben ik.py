94f8747f9ed157e49c9ba94dc5d1697023a8251f
JOONTJUUHS <71132375+JOONTJUUHS@users.noreply.github.com>
2020-10-30 00:40:22 +0100

    Add files via upload

---
+++ dit ben ik.py
@@ -0,0 +1,22 @@
+naam1 = "Guido"
+print("Hello you!, ik ben " + naam1)  
+print("Wie ben jij?")
+naam2 = input("vul hier je naam in ")
+print("Hello " + naam2)
+
+import datetime
+x = datetime.datetime.now()
+print("De tijd en datum is") 
+print(x)
+
+print("hoe oud denk je dat ik ben?")  
+varA = 18
+varB = 19
+varC = 20
+antwoord = varB
+input(" varA, varB, varC")
+def part_one () :
+    answer_one = raw_input ('>>> ').lower()
+    try: 
+        if answer_one == '19' print("goedzo dat is waar")
+         
